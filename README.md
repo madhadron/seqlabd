c92f3f49f442c50805924fddd933397fc5df307c
Fred Ross <madhadron@gmail.com>
2011-12-15 15:16:58 -0800

    Added setup.py, GPL, and skeleton of a README

---
+++ README.md
@@ -0,0 +1,14 @@
+seqlabd 0.1 - Analysis daemon for the clinical sequencing lab
+=============================================================
+
+by Fred Ross <fredross@uw.edu>
+
+Installation
+------------
+
+
+Configuration
+-------------
+
+Server configuration
+--------------------
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='seqlabd',
+      version='0.1',
+      description='Clinical Sequencing Lab Daemon',
+      author='Frederick J. Ross',
+      author_email='fredross@uw.edu',
+      url='',
+      packages=[],
+     )
