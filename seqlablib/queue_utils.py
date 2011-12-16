b10ceb29d78119f141943f67132fc3aaaca82842
Frederick Ross <fross@mephistopheles.(none)>
2011-12-16 14:31:21 -0800

    Added a few more queue utilities

--- seqlablib/queue_utils.py
+++ seqlablib/queue_utils.py
@@ -1,7 +1,8 @@
+import os
 import sets
 import threading
 #import pyinotify
-
+import Queue
 import refs
 
 def queue_events(queue, path, mask, fun=lambda x:x.path):
@@ -19,21 +20,43 @@ def queue_events(queue, path, mask, fun=lambda x:x.path):
     notifier.loop()
 
 
-def batched_unique(queue, fun, timeout_ref, n_batches=None):
+def batched_unique(queue, fun, timeout_ref, exit_event):
     state_ref = refs.Ref()
     state_ref.put(sets.ImmutableSet())
     def run_batch():
         batch = state_ref.put(sets.ImmutableSet())
-        threading.Thread(fun,(batch,)).run()
-        if n_batches:
-            n_batches -= 1
+        threading.Thread(target=fun,args=(batch,)).run()
     timer = None
-    while n_batches > 0:
-        v = queue.get(True, None)
-        state_ref.put(state_ref.get().union([v]))
-        if timer:
-            timer.cancel()
-        timer = threading.Timer(timeout_ref.get(), run_batch)
+    while True:
+        try:
+            v = queue.get(False, None)
+            state_ref.put(state_ref.get().union([v]))
+            if timer:
+                timer.cancel()
+            timer = threading.Timer(timeout_ref.get(), run_batch)
+            timer.start()
+        except Queue.Empty, e:
+            if exit_event.is_set():
+                return
+                
+def intermittently(fun, delay_ref, exit_event):
+    run_again = threading.Event()
+    run_again.set()
+    timer = None
+    while not(exit_event.is_set()):
+        if run_again.is_set():
+            run_again.clear()
+            threading.Thread(target=fun).start()
+            timer = threading.Timer(delay_ref.get(), lambda: run_again.set())
+            timer.start()
+    timer.cancel()
+
+def enqueue_files(queue, path_ref):
+    path = path_ref.get()
+    filenames = os.listdir(path)
+    for f in filenames:
+        queue.put(os.path.join(path, f))
+        
 
     
 
--- test/test_queue_utils.py
+++ test/test_queue_utils.py
@@ -1,3 +1,5 @@
+import common
+import os
 import threading
 import time
 import Queue
@@ -13,17 +15,48 @@ def test_batched_unique():
     q = Queue.Queue()
     timeout_ref = seqlablib.refs.Ref()
     timeout_ref.put(0.2)
-    e = threading.Event()
+    batchedevt = threading.Event()
+    exitevt = threading.Event()
     d = []
     def f(xs):
-        print "Running batch: %s" % str(xs)
         d.extend(xs)
-        e.set()
-    t = threading.Thread(target=seqlablib.queue_utils.batched_unique, args=(q, f, timeout_ref, 1))
+        batchedevt.set()
+    t = threading.Thread(target=seqlablib.queue_utils.batched_unique, args=(q, f, timeout_ref, exitevt))
     t.start()
     q.put(1); q.put(2); q.put(3)
-    e.wait()
+    batchedevt.wait()
     assert d==[1,2,3]
-    t.daemon = True
+    exitevt.set()
 
-test_batched_unique()
+def test_intermittently():
+    delay_ref = seqlablib.refs.Ref()
+    delay_ref.put(0.05)
+    d = []
+    evt = threading.Event()
+    exit_evt = threading.Event()
+    def f():
+        d.append(None)
+        evt.set()
+    threading.Thread(target=seqlablib.queue_utils.intermittently, args=(f, delay_ref, exit_evt)).start()
+    evt.wait()
+    assert d==[None]
+    evt.clear()
+    evt.wait()
+    assert d==[None,None]
+    evt.clear()
+    evt.wait()
+    assert d==[None,None,None]
+    evt.clear()
+    exit_evt.set()
+
+def test_enqueue_files():
+    q = Queue.Queue()
+    path_ref = seqlablib.refs.Ref()
+    path_ref.put('data/to_enqueue')
+    seqlablib.queue_utils.enqueue_files(q, path_ref)
+    d = []
+    while not(q.empty()):
+        x = q.get()
+        n = os.path.split(x)[1]
+        d.append(n)
+    assert d == ['alpha','beta','gamma']
