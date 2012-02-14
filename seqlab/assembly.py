eeb44383479a030fd6cf7fec98cfde37c6eee3d4
Frederick Ross <fross@mephistopheles.(none)>
2012-02-13 16:28:48 -0800

    Wrote new data structures for assembly.

---
+++ seqlab/assembly.py
@@ -0,0 +1,303 @@
+"""
+assembly.py - Data structures for assemblies
+
+This module provides data structures that make working with sequence
+assemblies simple. A particular track is specified as an AffineList
+(essentially, a list that knows where it begins in a 1D, discrete
+space and is indexed by global coordinates as opposed to its own
+indices. An assembly of tracks is stored in an Assembly, which
+provides methods to subset and iterate over hunks of tracks.
+"""
+
+import itertools as it
+import json
+import collections
+
+class HalfOpenInterval(object):
+    """Represents a half open interval [a,b), a <= b.
+
+    Working with AffineLists and Assemblies, we regularly want to
+    combine and compare various half open intervals. You can test if a
+    coordinate is in a HalfOpenInterval using Python's ``in``
+    command::
+
+        3 in HalfOpenInterval(2,6)
+
+    Or take the intersection with ``intersect``. To quickly get a
+    2-tuple (a,b), call the ``bounds`` method. The individual
+    coordinates are in the fields ``left`` and ``right``.
+
+    HalfOpenInterval does not provide union or complement methods
+    since the results of those need not be HalfOpenIntervals.
+    """
+    def __init__(self, a, b):
+        """Create a HalfOpenInterval [a,b)."""
+        assert a <= b
+        self.left = a
+        self.right = b
+    def __contains__(self, x):
+        """Calculate x in [a,b)."""
+        return self.left <= x and x < self.right
+    def __eq__(self, other):
+        return self.left == other.left and self.right == other.right
+    def __repr__(self):
+        return "HalfOpenInterval(%s,%s)" % (str(self.left), str(self.right))
+    def bounds(self):
+        """Return the HalfOpenInterval as a 2-tuple (a,b)."""
+        return (self.left, self.right)
+    def intersect(self, other):
+        """Intersect two HalfOpenIntervals.
+
+        Empty intersections are canonically set to HalfOpenInterval(0,0).
+        """
+        if self.left >= other.right or other.left >= self.right:
+            return HalfOpenInterval(0,0)
+        else:
+            left = max(self.left, other.left)
+            right = min(self.right, other.right)
+            if left == right:
+                return HalfOpenInterval(0,0)
+            else:
+                return HalfOpenInterval(left, right)
+
+
+class AffineList(object):
+    """List embedded in 1D, discrete, affine space.
+
+    Essentially, it's a list with an offset and methods altered to
+    work in terms of global coordinates instead of indices. Create it
+    by specifying an offset and an iterable of values, as in::
+
+        a = AffineList(offset=3, vals=[1,2,3,4,5])
+
+    Most of the usual list functions are available, but work a little
+    differently. ``index`` is replaced with ``find``, a more powerful
+    and general function.
+
+    Fetching items or slices is done with coordinates, not indices
+    into the list, so ``a[4]`` in the list above is 2. The first item
+    in the list is at the offset. Any items from outside the actual
+    list are returned as ``None``.
+
+    You can shift an AffineList left or right with the ``<<`` and
+    ``>>`` operators, so::
+
+        AffineList(3, [1]) << 1 == AffineList(2, [1])
+
+    The method ``support`` returns a HalfOpenInterval specifying where
+    the AffineList's items lie, and ``itercoords`` is equivalent to
+    ``enumerate()`` on a normal list, but returns the coordinates
+    instead of the indices.
+    """
+    def __init__(self, offset, vals):
+        self.offset = offset
+        self.vals = list(vals)
+    def __getitem__(self, i):
+        if i < self.offset or i >= self.offset+len(self.vals):
+            return None
+        else:
+            return self.vals[i - self.offset]
+    def __getslice__(self, i, j):
+        if i==None:
+            i = 0
+        if j==None:
+            j = self.offset + len(self.vals)
+        newoffset = max(self.offset-i, 0)
+        left = max(i-self.offset, 0)
+        right = max(j-self.offset, 0)
+        newvals = self.vals[left:right]
+        if newvals == []: # Must impose a canonical offset on empty lists
+            return AffineList(0, [])
+        else:
+            return AffineList(offset=newoffset, vals=newvals)
+    def __rshift__(self, i):
+        return AffineList(offset=self.offset+i, vals=self.vals)
+    def __lshift__(self, i):
+        return AffineList(offset=self.offset-i, vals=self.vals)
+    def support(self):
+        """Return the HalfOpenInterval containing the support of this list.
+
+        For example, ``AffineList(offset=1, vals=[1,2]).support()`` is
+        ``HalfOpenInterval(1,3)``.
+        """
+        return HalfOpenInterval(self.offset, self.offset + len(self.vals))
+    def iter(self, start=None, end=None):
+        """Return an iterator over the elements in the support of this list."""
+        return it.imap(lambda (a,b): b, self.itercoords(start=start, end=end))
+    def itercoords(self, start=None, end=None):
+        """Return an iterator over (coordinate,item) pairs in the support of this list.
+
+        You can specify *start* and *end* in coordinates to return
+        some other range. Any items returns outside of the support are
+        denoted ``None``.
+        """
+        if start == None:
+            start = self.offset
+        if end == None:
+            end = self.offset + len(self.vals)
+        for i in range(start, end):
+            yield (i,self[i])
+    def __repr__(self):
+        return 'AffineList(offset=%d, vals=%s)' % (self.offset, repr(self.vals))
+    def json(self, *args, **kwargs):
+        return json.dumps({'__AffineList': True, 'offset': self.offset, 'vals': self.vals})
+    def __len__(self):
+        return len(self.vals)
+    def append(self, x):
+        """Append *x* as an item to the end of this list."""
+        self.vals.append(x)
+        return self
+    def insert(self, i, x):
+        """Insert *x* at coordinate *i*.
+
+        The list is properly extended with ``None``s if *i* is outside
+        the list's support.
+        """
+        if i in self.support():
+            self.vals.insert(i-self.offset, x)
+        elif i < self.offset:
+            self.vals = [x] + [None]*(self.offset-i-1) + self.vals
+            self.offset = i
+        else: # i off to right of support
+            self.vals = self.vals + [None]*(i - (self.offset+len(self.vals))) + [x]
+        return self
+    def extend(self, vals):
+        """Append all items in *vals* to the end of the list."""
+        self.vals.extend(vals)
+        return self
+    def find(self, template, all=False, start=None, end=None):
+        """Find the coordinates matching *template*.
+
+        *template* may be a value or a callable object. If callable,
+        it is treated as a predicate. Specifying ``all=True`` makes
+        ``find`` return all coordinates where the template is matched.
+        Otherwise it returns only the first. *start* and *end* can be
+        used to restrict the search area.
+        """
+        if not(callable(template)):
+            f = lambda x: x==template
+        else:
+            f = template
+        if start == None:
+            start = self.support().left
+        if end == None:
+            end = self.support().right
+        
+        coords = []
+        for i in range(start, end):
+            if f(self[i]):
+                if all:
+                    coords.append(i)
+                else:
+                    return i
+        return coords
+    def __setitem__(self, i, x):
+        if i in self.support():
+            self.vals[i-self.offset] = x
+        elif i < self.offset:
+            self.vals = [x] + [None]*(self.offset-i-1) + self.vals
+            self.offset = i
+        else: # i off to right of support
+            self.vals = self.vals + [None]*(i - (self.offset+len(self.vals))) + [x]
+        return x
+    def __eq__(self, other):
+        return self.offset == other.offset and self.vals == other.vals
+
+class Assembly(collections.OrderedDict):
+    """Class representing an assembly of sequences.
+
+    ``Assembly`` is an extension of an ordered dictionary. All its
+    values should be ``AffineList`` objects. It provides numerous
+    methods to make working with such collections simple. It also has
+    a slot ``metadata`` to store information about the assembly. The
+    ``metadata`` is properly propogated through all operations on
+    ``Assembly``.
+    """
+    def __init__(self, items, metadata={}):
+        collections.OrderedDict.__init__(self, items)
+        self.metadata = metadata
+    def filterkeys(self, pred):
+        """Return an Assembly containing only the items for which *pred*(key) is true."""
+        return Assembly([(k,v) for k,v in self.iteritems() if pred(k)], metadata=self.metadata)
+    def filteritems(self, pred):
+        """Return an Assembly containing only the items for which *pred*(key,value) is true."""
+        return Assembly([(k,v) for k,v in self.iteritems() if pred(k,v)], metadata=self.metadata)
+    def filtervalues(self, pred):
+        """Return an Assembly containing only the items for which *pred*(value) is true."""
+        return Assembly([(k,v) for k,v in self.iteritems() if pred(v)], metadata=self.metadata)
+    def mapkeys(self, fun):
+        """Return an Assembly where the keys are replaced by *fun*(key,val)."""
+        return Assembly([(fun(k,v), v) for k,v in self.iteritems()], metadata=self.metadata)
+    def mapitems(self, fun):
+        """Return an Assembly with items replaced by 2-tuples returned by *fun*(key,val)."""
+        return Assembly([fun(k,v) for k,v in self.iteritems()], metadata=self.metadata)
+    def mapvalues(self, fun):
+        """Return an Assembly with values replaced by *fun*(key,value)."""
+        return Assembly([(k,fun(k,v)) for k,v in self.iteritems()], metadata=self.metadata)
+    def __add__(self, other):
+        """Append the entries in *other* to this.
+        
+        There must not be any duplicate keys.
+        """
+        return Assembly([(k,v) for k,v in self.iteritems()] + \
+                            [(k,v) for k,v in other.iteritems()], metadata=self.metadata)
+    def support(self, *keys):
+        """Return the support of the intersection of *keys*.
+
+        If there are no arguments (keys is ``[]``), returns the
+        HalfOpenInterval of the support of all data in the assembly.
+        Otherwise, the keys are taken to be keys into the dict, and
+        the HalfOpenInterval of the intersection of the support of
+        each key's value is returned.
+        """
+        v = HalfOpenInterval(min([v.offset for v in self.itervalues()] or [0]),
+                             max([v.offset+len(v) for v in self.itervalues()] or [0]))
+        for k in keys:
+            v = v.intersect(self[k].support())
+        return v
+    def itercolumns(self, start=None, end=None):
+        """Iterate over columns in the Assembly.
+
+        Each column is yielded as a 2-tuple of
+        (coordinate,OrderedDict), where OrderedDict has the same keys
+        as the original, but with a single item as its key. If *start*
+        and *end* are omitted, it iterates over the whole support of
+        the ``Assembly``.
+        """
+        whole = self.support()
+        if start == None:
+            start = whole.left
+        if end == None:
+            end = whole.right
+        for i in range(start,end):
+            yield (i,collections.OrderedDict([(k,v[i]) for k,v in self.iteritems()]))
+    def subset(self, start=0, end=None):
+        """Return an Assembly of a subset of columns.
+
+        If *start* and *end* are omitted, returns the Assembly itself.
+        Otherwise, returns an Assembly with its origin at *start* and
+        with all the columns out to *end*.
+        """
+        if isinstance(start, HalfOpenInterval):
+            end = start.right
+            start = start.left
+        if end == None:
+            end = self.support().right
+        return Assembly([(k, v[start:end]) for k,v in self.iteritems()], metadata=self.metadata)
+    def narrowto(self, *keys):
+        """Return an Assembly subset to the support of *keys*.
+
+        *keys* is specified in the same way as for the ``support`` method.
+        """
+        print self.support(*keys)
+        return self.subset(self.support(*keys))
+    def width(self, *keys):
+        """Return the width of the support of *keys* in the Assembly.
+
+        If *keys* is empty, then the width of the whole assembly.
+        *keys* is specified as for the ``support`` method.
+        """
+        s = self.support(*keys)
+        return s.right - s.left
+        
+    
---
+++ test/test_assembly.py
@@ -0,0 +1,180 @@
+import common
+from seqlab.assembly import *
+
+def test_halfopeninterval_intersect():
+    assert HalfOpenInterval(1,3).intersect(HalfOpenInterval(5,7)) == HalfOpenInterval(0,0)
+    assert HalfOpenInterval(1,3).intersect(HalfOpenInterval(1,2)) == HalfOpenInterval(1,2)
+
+def test_affinelist_getitem():
+    a = AffineList(offset=3, vals=[1])
+    assert a[2] == None
+    assert a[3] == 1
+    assert a[4] == None
+
+def test_affinelist_getslice():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])
+    assert a[0:4] == AffineList(3, [1])
+    assert a[0:6] == AffineList(3, [1,2,3])
+    assert a[4:6] == AffineList(0, [2,3])
+    assert a[4:25] == AffineList(0, [2,3,4,5])
+    assert a[25:28] == AffineList(0, [])
+    assert a[0:2] == AffineList(0, [])
+
+def test_affinelist_shifts():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])
+    assert a << 3 == AffineList(0, [1,2,3,4,5])
+    assert a >> 2 == AffineList(5, [1,2,3,4,5])
+    assert a << 0 == a
+    assert a >> 0 == a
+    assert a >> 1 == a << -1
+    assert a << 1 == a >> -1
+
+def test_affinelist_support():
+    assert AffineList(3, [1,2,3,4,5]).support() == HalfOpenInterval(3,8)
+    assert AffineList(0, []).support() == HalfOpenInterval(0,0)
+    assert AffineList(1, []).support() == HalfOpenInterval(1,1)
+    assert AffineList(1, [3]).support() == HalfOpenInterval(1,2)
+
+def test_affinelist_iter():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])    
+    assert list(a.iter()) == [1,2,3,4,5]
+    assert list(a.iter(start=0)) == [None,None,None,1,2,3,4,5]
+    assert list(a.iter(start=0, end=2)) == [None,None]
+    assert list(a.iter(start=0, end=9)) == [None,None,None,1,2,3,4,5,None]
+
+def test_affinelist_itercoords():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])    
+    assert list(a.itercoords()) == [(3,1),(4,2),(5,3),(6,4),(7,5)]
+    assert list(a.itercoords(start=0)) == [(0,None),(1,None),(2,None),(3,1),
+                                           (4,2),(5,3),(6,4),(7,5)]
+    assert list(a.itercoords(start=0, end=2)) == [(0,None),(1,None)]
+    assert list(a.itercoords(start=0, end=9)) == [(0,None),(1,None),(2,None),(3,1),(4,2),
+                                            (5,3),(6,4),(7,5),(8,None)]
+
+def test_affinelist_repr():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])    
+    assert eval(repr(a)) == a
+
+def test_affinelist_json():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])
+    assert json.loads(a.json()) == {"__AffineList": True, "offset": 3, "vals": [1,2,3,4,5]}
+
+def test_affinelist_append():
+    a = AffineList(offset=3, vals=[1,2,3,4,5])
+    assert a.append(1) == AffineList(3, [1,2,3,4,5,1])
+
+def test_affinelist_insert():
+    assert AffineList(3, [1,2,3,4,5]).insert(3,0) == AffineList(3, [0,1,2,3,4,5])
+    assert AffineList(0, []).insert(0, 12) == AffineList(0, [12])
+    assert AffineList(3, [1,2]).insert(1, 5) == AffineList(1, [5,None,1,2])
+    assert AffineList(3, [1,2]).insert(7, 13) == AffineList(3, [1,2,None,None,13])
+
+def test_affinelist_find():
+    assert AffineList(3, [1,2,3,4,5]).find(lambda x: x%2==0, all=True) == [4,6]
+    assert AffineList(3, [1,2,3,4,5]).find(lambda x: x%2==0) == 4
+    assert AffineList(3, [1,2,1,2,1]).find(1, all=True) == [3,5,7]
+    assert AffineList(3, [1,2,1,2,1]).find(1) == 3
+    assert AffineList(3, [1,2,1,2,1]).find(1, all=True, start=4) == [5,7]
+    assert AffineList(3, [1,2,1,2,1]).find(1, all=True, end=5) == [3]
+
+def test_affinelist_setitem():
+    a = AffineList(3, [1,2,3,4,5]); a[3] = 0
+    assert a == AffineList(3, [0,2,3,4,5])
+    a = AffineList(3, [1,2,3,4,5]); a[1] = 0
+    assert a == AffineList(1, [0,None,1,2,3,4,5])
+    a = AffineList(3, [1,2,3,4,5]); a[9] = 0
+    assert a == AffineList(3, [1,2,3,4,5,None,0])
+
+def test_assembly_init():
+    a = Assembly([('qboris', AffineList(0,[])),
+                  ('hilda', AffineList(0,[]))])
+    assert list(a.iteritems()) == [('qboris', AffineList(0, [])),
+                                   ('hilda', AffineList(0, []))]
+
+def test_assembly_filters():
+    a = Assembly([('ax', AffineList(3, [1,2,3])),
+                  ('ay', AffineList(3, [2,5,6])),
+                  ('bx', AffineList(0, [1,1,1]))])
+    assert a.filterkeys(lambda k: k.startswith('a')) == \
+        Assembly([('ax', AffineList(3, [1,2,3])),
+                  ('ay', AffineList(3, [2,5,6]))])
+    assert a.filteritems(lambda k,v: 5 in v.support()) == \
+        Assembly([('ax', AffineList(3, [1,2,3])),
+                  ('ay', AffineList(3, [2,5,6]))])
+    assert a.filtervalues(lambda v: 5 in v.support()) == \
+        Assembly([('ax', AffineList(3, [1,2,3])),
+                  ('ay', AffineList(3, [2,5,6]))])
+
+def test_assembly_maps():
+    a = Assembly([('ax', AffineList(3, [1,2,3])),
+                  ('ay', AffineList(3, [2,5,6])),
+                  ('bz', AffineList(0, [1,1,1]))])
+    assert a.mapkeys(lambda k,v: k[1]) == \
+        Assembly([('x', AffineList(3, [1,2,3])),
+                  ('y', AffineList(3, [2,5,6])),
+                  ('z', AffineList(0, [1,1,1]))])
+    assert a.mapitems(lambda k,v: (k,v[:5])) == \
+        Assembly([('ax', AffineList(3, [1,2])),
+                  ('ay', AffineList(3, [2,5])),
+                  ('bz', AffineList(0, [1,1,1]))])
+    assert a.mapvalues(lambda k,v: v[:5]) == \
+        Assembly([('ax', AffineList(3, [1,2])),
+                  ('ay', AffineList(3, [2,5])),
+                  ('bz', AffineList(0, [1,1,1]))])
+
+def test_assembly_adding():
+    a = Assembly([('ax', AffineList(0,[]))])
+    b = Assembly([('bx', AffineList(0,[]))])
+    assert a+b == Assembly([('ax', AffineList(0,[])),
+                            ('bx', AffineList(0,[]))])
+
+def test_assembly_support():
+    a = Assembly([('a', AffineList(1, [1,2,3,4,5])),
+                  ('b', AffineList(4, [1,2,3,4,5]))])
+    assert a.support() == HalfOpenInterval(1,9)
+    assert a.support('a') == HalfOpenInterval(1,6)
+    assert a.support('a', 'b') == HalfOpenInterval(4,6)
+
+def test_assembly_itercolumns():
+    a = Assembly([('a', AffineList(1, [1,2,3,4,5])),
+                  ('b', AffineList(4, [1,2,3,4,5]))])
+    assert list(a.itercolumns()) == [(1, collections.OrderedDict([('a',1),('b',None)])),
+                                     (2, collections.OrderedDict([('a',2),('b',None)])),
+                                     (3, collections.OrderedDict([('a',3),('b',None)])),
+                                     (4, collections.OrderedDict([('a',4),('b',1)])),
+                                     (5, collections.OrderedDict([('a',5),('b',2)])),
+                                     (6, collections.OrderedDict([('a',None),('b',3)])),
+                                     (7, collections.OrderedDict([('a',None),('b',4)])),
+                                     (8, collections.OrderedDict([('a',None),('b',5)]))]
+    assert list(a.itercolumns(start=3,end=5)) == \
+        [(3, collections.OrderedDict([('a',3),('b',None)])),
+         (4, collections.OrderedDict([('a',4),('b',1)]))]
+
+def test_assembly_subset():
+    a = Assembly([('a', AffineList(1, [1,2,3,4,5])),
+                  ('b', AffineList(4, [1,2,3,4,5]))])    
+    assert a.subset(start=3,end=5) == \
+        Assembly([('a', AffineList(0, [3,4])),
+                  ('b', AffineList(1, [1]))])
+    assert a.subset() == a
+    assert a.subset(HalfOpenInterval(3,5)) == \
+        Assembly([('a', AffineList(0, [3,4])),
+                  ('b', AffineList(1, [1]))])
+
+def test_assembly_narrowto():
+    a = Assembly([('a', AffineList(1, [1,2,3,4,5])),
+                  ('b', AffineList(4, [1,2,3,4,5]))])
+    assert a.narrowto() == Assembly([('a', AffineList(0, [1,2,3,4,5])),
+                                     ('b', AffineList(3, [1,2,3,4,5]))])
+    assert a.narrowto('a') == Assembly([('a', AffineList(0, [1,2,3,4,5])),
+                                        ('b', AffineList(3, [1,2]))])
+    assert a.narrowto('a','b') == Assembly([('a', AffineList(0, [4,5])),
+                                            ('b', AffineList(0, [1,2]))])
+    
+def test_assembly_width():
+    a = Assembly([('a', AffineList(1, [1,2,3,4,5])),
+                  ('b', AffineList(4, [1,2,3,4,5]))])
+    assert Assembly([]).width() == 0
+    assert a.width() == 8
+    assert a.width('a') == 5
+    assert a.width('a','b') == 2
