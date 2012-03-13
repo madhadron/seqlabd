9380dd9235a50b2e3350dd5ec5b568ba2b5a1594
Fred Ross <madhadron@gmail.com>
2012-03-13 13:45:50 -0700

    renderab1 command and other changes that didn't get committed

---
+++ seqlab/subcommands/renderab1.py
@@ -0,0 +1,29 @@
+"""Template for a subcommand."""
+
+import subprocess
+import tempfile
+import logging
+import shutil
+import os
+import sys
+import seqlab.ab1
+from seqlab.assembly import ab1tohtml
+
+log = logging.getLogger(__name__)
+
+def build_parser(parser):
+    parser.add_argument('ab1',
+        help='AB1 of file to render')
+    parser.add_argument('-o', '--output',
+        action='store', default=None,
+        help='filename to write HTML to')
+
+def action(args):
+    s = ab1tohtml(args.ab1)
+    if args.output:
+        with open(args.output, 'w') as out:
+            print >>out, s
+    else:
+        print s
+    return 0
+
---
+++ test/test_ab1.py
@@ -0,0 +1,19 @@
+import common
+from seqlab.ab1 import *
+
+def test_sparsify():
+    xs = numpy.arange(14)
+    ys = numpy.arange(14)/5.0
+    assert psparsify(xs,ys) == [(0,0), (11,11/5.0), (13,13/5.0)]
+    xs = numpy.arange(4)
+    ys = numpy.arange(4)
+    assert psparsify(xs,ys) == [(0,0), (3,3)]
+
+
+def test_cutoff():
+    xs = numpy.arange(50)
+    assert pcutoff(xs) == 40
+    assert pcutoff(numpy.array([2]*5 + [10]*50 + [40]*4)) == 10
+
+if __name__ == '__main__':
+    test_cutoff()
