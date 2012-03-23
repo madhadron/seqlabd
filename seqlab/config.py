893ecdcb237bde3340ae108f4770a6bcbc2bec3e
Fred Ross <madhadron@gmail.com>
2012-03-23 12:29:40 -0700

    Finished cleaning up configuration

--- seqlab/config.py
+++ seqlab/config.py
@@ -3,35 +3,22 @@ import ConfigParser
 
 def read_configuration(handle):
     default = \
-        {'batch_timeout': '15', 'slurp_timeout': '600',
-         'share_path': None, 'base_path': None, 'inbox_path': None,
+        {'share_path': None, 'base_path': None, 'inbox_path': None,
          'db_server': None, 'db_username': None,
          'db_port': '3306', 'db_credentials': None,
-         'db_name': None, 'daemon_user': None, 'daemon_group': None}
+         'db_name': None}
 
     scp = ConfigParser.SafeConfigParser(default)
     scp.readfp(handle)
-    conf = {'batch_timeout': scp.getint('default','batch_timeout'),
-            'slurp_timeout': scp.getint('default','slurp_timeout'),
-            'db_port': scp.getint('default','db_port'),
+    conf = {'db_port': scp.getint('default','db_port'),
             'db_name': scp.get('default', 'db_name'),
             'share_path': scp.get('default','share_path'),
             'base_path': scp.get('default','base_path'),
             'inbox_path': scp.get('default','inbox_path'),
-            'unmatched_path': scp.get('default','unmatched_path'),
             'db_server': scp.get('default','db_server'),
             'db_username': scp.get('default','db_username'),
-            'db_credentials': scp.get('default', 'db_credentials'),
-            'max_retries': scp.getint('default', 'max_retries'),
-            'daemon_user': scp.get('default', 'daemon_user'),
-            'daemon_group': scp.get('default', 'daemon_group')}
+            'db_credentials': scp.get('default', 'db_credentials')}
     target_path = os.path.join(conf['share_path'], conf['base_path'])
-    if conf['batch_timeout'] < 1:
-        raise ValueError("batch_timeout must be a positive integer, found %d" \
-                             % conf['batch_timeout'])
-    if conf['slurp_timeout'] < 1:
-        raise ValueError("slurp_timeout must be a positive integer, found %d" \
-                             % conf['slurp_timeout'])
     if not(os.path.isdir(target_path)):
         raise ValueError("No such path: %s" % target_path)
     if not(os.path.isdir(conf['inbox_path'])):
@@ -44,7 +31,7 @@ def read_configuration(handle):
         with open(conf['db_credentials']) as h:
             conf['db_password'] = h.readline().strip()
     except OSError, e:
-        raise ValueError("Couldn't read password from crednetials file %s" % \
+        raise ValueError("Couldn't read password from credentials file %s" % \
                              conf['db_credentials'])
     return conf
             
--- test/test_config.py
+++ test/test_config.py
@@ -6,19 +6,12 @@ import cStringIO
 
 conf_lines = [
     "[default]",
-    "# All times are measured in seconds by default.",
-    "batch_timeout=20",
-    "slurp_timeout=900",
     "share_path = /var",
     "base_path = log",
     "inbox_path = /usr/bin",
-    "unmatched_path = /usr/lib",
-    "# The password must be handled by a .pgpass file or some other method",
     "db_server = localhost",
     "db_port = 5432",
     "db_username = boris",
-    "# How many time to retry something that didn't work out.",
-    "max_retries = 5",
     "db_credentials = data/dbcredential",
     "db_name = mdx"]
 
@@ -26,23 +19,18 @@ conf_lines = [
 def test_good_config_works():
     good_config = cStringIO.StringIO('\n'.join(conf_lines))
     assert config.read_configuration(good_config) == \
-        {'batch_timeout': 20, 'slurp_timeout': 900,
-         'share_path': '/var', 'base_path': 'log',
-         'inbox_path': '/usr/bin', 'unmatched_path': '/usr/lib',
+        {'share_path': '/var', 'base_path': 'log',
+         'inbox_path': '/usr/bin', 
          'db_port': 5432, 'db_name': 'mdx', 
-         'db_server': 'localhost', 'db_username': 'boris', 'max_retries': 5,
+         'db_server': 'localhost', 'db_username': 'boris', 
          'db_credentials': 'data/dbcredential', 'db_password': 'root',
-         'daemon_group': None, 'daemon_user': None, 'target_path': '/var/log'}
+         'target_path': '/var/log'}
 
 
 def test_bad_values_fail():
     for i,s in [(4,"share_path = /wasdfkshdf"),
                 (5,"base_path = sdfkjshfdf/weraksdfhjds"),
-                (6,"inbox_path = /dfsdfhljfwe/wefsdfh"),
-                (7,"unmatched_path = /asdfsdhdsfsd/wasdfhfs"),
-                (2,"batch_timeout=-51"),
-                (3,"slurp_timeout=-3"),
-                (13,"max_retries=-5")]:
+                (6,"inbox_path = /dfsdfhljfwe/wefsdfh")]:
         new_lines = copy.copy(conf_lines)
         new_lines[i] = s
         h = cStringIO.StringIO('\n'.join(new_lines))
